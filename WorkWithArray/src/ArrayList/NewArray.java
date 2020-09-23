23e30da9866e120e02f49ad86bff53e3df452790
eibio <eibiologos@gmail.com>
2020-09-24 00:18:17 +0300

    Change array to private.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -1,9 +1,10 @@
 package ArrayList;
 
 public class NewArray {
+    private static int[] array = {23, 45, 87, 4, 7, 87, 55, 34, 123, 15, 6};
+    private static int[] array2 = {77, 95, 7, 4, 34, 66, 3, 38, 41, 66, 9, 16, 5, 2, 86};
+
     public static void main(String[] args) {
-        int [] array = {23,45,87,4,7,87,55,34,123,15,6};
-        int [] array2 = {77,95,7,4,34,66,3,38,41,66,9,16,5,2,86};
         array[0] = 1;
         System.out.println(array[0]);
         System.out.println(array.length);
@@ -28,7 +29,7 @@ public class NewArray {
         }
 
 
-        System.out.println("Max element: " + max);
+        System.out.println("Max element " + max);
         System.out.println("Max element index : " + maxIndex);
         System.out.println("Comparisons: " + comparisons);
         System.out.println("Replacements: " + replacements);
