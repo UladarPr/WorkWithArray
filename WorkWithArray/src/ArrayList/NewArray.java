d3de1626d46b07c26b3af13de318ae02d98a27e6
eibio <eibiologos@gmail.com>
2020-09-23 23:58:18 +0300

    Add new array.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -3,9 +3,11 @@ package ArrayList;
 public class NewArray {
     public static void main(String[] args) {
         int [] array = {23,45,87,4,7,87,55,34,123,15,6};
+        int [] array2 = {77,95,7,4,34,66,3,38,41,66,9,16,5,2,86};
         array[0] = 1;
         System.out.println(array[0]);
         System.out.println(array.length);
+        System.out.println(array2.length);
     }
 
 
