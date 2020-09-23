f06a5aea45ce314d53516070bb29a41e8cc9931c
eibio <eibiologos@gmail.com>
2020-09-24 00:19:31 +0300

    Change array to private.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -1,8 +1,9 @@
 package ArrayList;
 
 public class NewArray {
+   private static int[] array = {23, 45, 87, 4, 7, 87, 55, 34, 123, 15, 6};
     public static void main(String[] args) {
-        int[] array = {23, 45, 87, 4, 7, 87, 55, 34, 123, 15, 6};
+
         array[0] = 1;
         System.out.println(array[0]);
         System.out.println(array.length);
