c8fa9920a1825c317bf7859b78ae7aca0adff4f2
eibio <eibiologos@gmail.com>
2020-09-23 23:42:20 +0300

    change variable name, change first component, add methode main.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -1,5 +1,10 @@
 package ArrayList;
 
 public class NewArray {
-    private int [] arr = {23,45,87,4,7,87,55,34,123,15,6};
+    public static void main(String[] args) {
+        int [] array = {23,45,87,4,7,87,55,34,123,15,6};
+        array[0] = 1;
+    }
+
+
 }
