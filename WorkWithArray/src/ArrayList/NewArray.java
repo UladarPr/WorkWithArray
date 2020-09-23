3df51915e993cece061f4c238408b9145bae0fd1
eibio <eibiologos@gmail.com>
2020-09-23 23:54:28 +0300

    Check array length.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -4,6 +4,8 @@ public class NewArray {
     public static void main(String[] args) {
         int [] array = {23,45,87,4,7,87,55,34,123,15,6};
         array[0] = 1;
+        System.out.println(array[0]);
+        System.out.println(array.length);
     }
 
 
