0d21150b40b4a406635b1d47dc4068b45588656d
eibio <eibiologos@gmail.com>
2020-09-24 00:15:26 +0300

    Add method maximumElement.

--- WorkWithArray/src/ArrayList/NewArray.java
+++ WorkWithArray/src/ArrayList/NewArray.java
@@ -8,7 +8,30 @@ public class NewArray {
         System.out.println(array[0]);
         System.out.println(array.length);
         System.out.println(array2.length);
+        printMaximumElement(array);
+        printMaximumElement(array2);
     }
 
+    static void printMaximumElement(int[] arr) {
+        int max = arr[0];
+        int maxIndex = 0;
+        int comparisons = 0;
+        int replacements = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+                replacements++;
 
+            }
+            comparisons++;
+        }
+
+
+        System.out.println("Max element: " + max);
+        System.out.println("Max element index : " + maxIndex);
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Replacements: " + replacements);
+
+    }
 }
